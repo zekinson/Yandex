6e2a23c82df0e03c84c78787a5704179fde6f544
zekinson <zekagorhakov@gmail.com>
2023-12-13 19:07:14 +0600

    commit

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Gallows(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        pass
+
+    def paintEvent(self, e):
+        pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    window = Gallows()
+    window.show()
+    sys.exit(app.exec_())
